d9d5ed23516a21e0a3806dc16a6608ab482b4752
Andrey <andrey.develop.a@gmail.com>
2023-10-07 21:18:18 +0700

    files

---
+++ views/layouts/login.php
@@ -0,0 +1,31 @@
+<?php
+/** @var yii\web\View $this */
+/** @var string $content */
+
+use app\assets\AppAsset;
+
+AppAsset::register($this);
+
+?>
+
+<?php $this->beginPage() ?>
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <title>Login</title>
+        <?php $this->head() ?>
+    </head>
+    <body>
+    <?php
+    ?>
+    <?php $this->beginBody() ?>
+    <?= $content ?>
+    <?php $this->endBody() ?>
+    </body>
+    </html>
+<?php $this->endPage() ?>
