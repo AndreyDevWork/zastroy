bbf169f23aeb151f65de02d2e1b5473de7dbee78
Andrey <andrey.develop.a@gmail.com>
2023-10-11 01:36:34 +0700

    fix

--- web/css/ui-kit/select/select.js
+++ web/css/ui-kit/select/select.js
@@ -1,36 +1,36 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var selects = document.querySelectorAll('.select');
+    let selects = document.querySelectorAll('.select');
     selects.forEach(function(select) {
-        var selectOption = select.querySelectorAll('option');
-        var selectOptionLength = selectOption.length;
-        var selectedOption = Array.from(selectOption).find(option => option.selected);
-        var duration = 450; // длительность анимации
+        let selectOption = select.querySelectorAll('option');
+        let selectOptionLength = selectOption.length;
+        let selectedOption = Array.from(selectOption).find(option => option.selected);
+        let duration = 450; // длительность анимации
 
         select.style.display = 'none';
-        var selectWrapper = document.createElement('div');
+        let selectWrapper = document.createElement('div');
         selectWrapper.className = 'select';
         select.parentNode.insertBefore(selectWrapper, select);
         selectWrapper.appendChild(select);
 
-        var newSelect = document.createElement('div');
+        let newSelect = document.createElement('div');
         newSelect.className = 'new-select';
         newSelect.textContent = Array.from(select.children).find(option => option.disabled).textContent;
         selectWrapper.appendChild(newSelect);
 
-        var selectList = document.createElement('div');
+        let selectList = document.createElement('div');
         selectList.className = 'new-select__list';
         selectWrapper.appendChild(selectList);
 
-        for (var i = 1; i < selectOptionLength; i++) {
-            var item = document.createElement('div');
+        for (let i = 1; i < selectOptionLength; i++) {
+            let item = document.createElement('div');
             item.className = 'new-select__item';
             item.innerHTML = '<span>' + selectOption[i].textContent + '</span>';
             item.setAttribute('data-value', selectOption[i].value);
             selectList.appendChild(item);
         }
 
-        var selectHead = selectWrapper.querySelector('.new-select');
-        var selectItems = selectList.querySelectorAll('.new-select__item');
+        let selectHead = selectWrapper.querySelector('.new-select');
+        let selectItems = selectList.querySelectorAll('.new-select__item');
         selectList.style.display = 'none';
 
         selectHead.addEventListener('click', function() {
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
                 selectItems.forEach(function(item) {
                     item.addEventListener('click', function() {
-                        var chooseItem = item.getAttribute('data-value');
+                        let chooseItem = item.getAttribute('data-value');
                         select.value = chooseItem;
                         select.setAttribute('selected', 'selected');
                         selectHead.textContent = item.querySelector('span').textContent;
