d3e2367de7e8d4e7a96926d9e00b782b28544d9b
Верстальщик <andrey.develop.a@gmail.com>
2023-10-19 16:12:08 +0700

    fix

--- views/site/edit-leader.php
+++ views/site/edit-leader.php
@@ -60,7 +60,7 @@ use yii\bootstrap5\Html;
                         </div>
                         <div class="contact__edit-pass">
                             <?= $form->field($model, 'passworddd')->textInput(['autofocus' => false, 'class' => 'input contact__city'])->label('Пароль<span class="input__required">*</span>') ?>
-                            <div class="my-btn opacity-btn title16 contact__opacity-btn">Cгенерировать пароль</div>
+                            <div class="my-btn opacity-btn contact__opacity-btn">Cгенерировать пароль</div>
                         </div>
                         <div class="contact__edit-btns">
                             <?= Html::submitButton('<img src="/web/img/icon-svg/save.svg" alt="save">Сохранить', ['class' => 'my-btn contact__save-btn', 'name' => 'login-button']) ?>
